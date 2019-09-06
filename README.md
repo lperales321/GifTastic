5c962d9184e4243a35e40dc86dab418b8e69b092
Lorena Perales <lperales321@gmail.com>
2019-09-05 22:20:46 -0500

    fix button click

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # GifTastic
-Dynamic web page that displays GIFs for a topic
+A pre-defined group of buttons is displayed with horror movie characters.
+When you click a button, ten images are returned.
+Add another character and click the Submit button to add it to the list.
+
+When you click on an image, it will be animated.
+When you click on it again, it will stop.
+
+NOTE: The image onClick event is not getting triggered when clicking on an image.
+The image does have the class used on the onClick event. I am not sure why it's not triggering.
+I think it has to do with the ordering of the function and events.
+
+I did add the logic to swap between animate and still image, but cannot trigger it.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,81 +6,87 @@ const topics = ["Pennywise", "Freddy Krueger", "Jason Voorhees", "Leatherface", 
 // Function for displaying topic buttons
 function renderButtons() {
 
-    // Delete the buttons prior to adding new topics
-    $("#buttons").empty();
-
     // Looping through the array of movies
     for (let i = 0; i < topics.length; i++) {
         let personBtn = $("<button>");
         personBtn.attr("data-person", topics[i]);
         personBtn.text(topics[i]);
+        personBtn.addClass("gifBtn")
     
         $("#buttons").append(personBtn);
     };
+
+    $(".gifBtn").on("click", function(event) {
+        event.preventDefault();
+    
+        let person = $(this).data("person");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            let results = response.data;
+    
+            for (let i = 0; i < results.length; i++) {
+                let gifDiv = $("<div>");
+    
+                let rating = results[i].rating;
+    
+                let p = $("<p>").text("Rating: " + rating);
+    
+                let personImage = $("<img>");
+                personImage.addClass("gifImage");
+                personImage.attr("src", results[i].images.fixed_height_still.url);
+                personImage.attr("data-still", results[i].images.fixed_height_still.url);
+                personImage.attr("data-animate", results[i].images.fixed_height.url);
+                personImage.attr("data-state", "still");
+    
+                gifDiv.prepend(p);
+                gifDiv.prepend(personImage);
+    
+                $("#gifs-appear-here").prepend(gifDiv);
+            }
+        }).catch(function(error) {
+            console.log(error.code);
+        });
+    });
+
+    $(".gifImage").on("click", function(event) {
+        event.preventDefault();
+    
+        let state = $(this).attr("data-state");
+    
+        if(state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+    // This function handles events where submit button is clicked
+    $("#add-topic").on("click", function(event) {
+        event.preventDefault();
+
+        // Delete the buttons prior to adding new topics
+        $("#buttons").empty();
+
+        // This line grabs the input from the textbox
+        let character = $("#topic-input").val().trim();
+
+        // The character from the textbox is then added to the array
+        topics.push(character);
+
+        // Calling renderButtons which handles the processing of the topic array
+        renderButtons();
+
+        $("#topic-input").val("");
+    });
 }
+
 
 //Calling the renderButtons function to display the intial buttons
 renderButtons();
-
-$("button").on("click", function() {
-    event.preventDefault();
-
-    let person = $(this).data("person");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        let results = response.data;
-
-        for (let i = 0; i < results.length; i++) {
-            let gifDiv = $("<div>");
-
-            let rating = results[i].rating;
-
-            let p = $("<p>").text("Rating: " + rating);
-
-            let personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height_still.url);
-            personImage.attr("data-still", results[i].images.fixed_height_still.url);
-            personImage.attr("data-animate", results[i].images.fixed_height.url);
-            personImage.attr("data-state", "still");
-            personImage.addClass("gif");
-
-            gifDiv.prepend(p);
-            gifDiv.prepend(personImage);
-
-            $("#gifs-appear-here").prepend(gifDiv);
-        }
-    }).catch(function(error) {
-        console.log(error.code);
-    });
-});
-
-// This function handles events where submit button is clicked
-$("#add-topic").on("click", function(event) {
-    event.preventDefault();
-
-    // This line grabs the input from the textbox
-    let character = $("#topic-input").val().trim();
-
-    // The character from the textbox is then added to the array
-    topics.push(character);
-
-    // Calling renderButtons which handles the processing of the topic array
-    renderButtons();
-});
-
-$(".gif").on("click", function() {
-    let state = $(this).attr("data-state");
-
-    if(state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-    }
-    else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-    }
-});
